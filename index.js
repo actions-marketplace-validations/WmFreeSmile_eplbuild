864f1635baafaeaf1b476e5b664ac80e782d2c99
WmFreeSmile <wmfreesmile@outlook.com>
2025-07-01 11:49:56 +0800

    change

--- index.js
+++ index.js
@@ -5,15 +5,16 @@ const { execFile } = require('child_process');
 
 const exePath = __dirname+'/bin/eplc.exe';
 
-var args = ['', '/o', '', '/obj', '/dll', '/set_epl_root', './bin/'];
+var args = ['', '/o', '', '/obj', '/dll', '/set_epl_root', ''];
 
 //获取变量
 const file = core.getInput('file');
 const output = core.getInput('output');
 
 //设置命令行
-args[0]=file;
-args[2]=output;
+args[0] = file;
+args[2] = output;
+args[6] = __dirname+'/bin/';
 
 //运行可执行文件
 const child = execFile(exePath, args, (error, stdout, stderr) => {
